0251a12ede91eade162d240b46af02c54aae01e0
Chemical Warehouse Admin <admin@chemicalware.com>
2024-01-22 11:54:54 +0200

    Added REST API Integration Link and Code for REST API.

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Member;
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticationController extends Controller
+{
+    //this method adds new users
+    public function createAccount(Request $request)
+    {
+        $attr = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $attr['name'],
+            'password' => bcrypt($attr['password']),
+            'email' => $attr['email']
+        ]);
+        return \Illuminate\Support\Facades\Response::json([
+            'token' => $user->createToken('tokens')->plainTextToken
+
+        ]);
+    }
+    //use this method to signin users
+    public function signin(Request $request)
+    {
+        $attr = $request->validate([
+            'email' => 'required|string|email|',
+            'password' => 'required|string|min:6'
+        ]);
+
+        if (!Auth::attempt($attr)) {
+            return \Illuminate\Support\Facades\Response::json(["error"=>'Credentials not match'], 401);
+        }
+        return \Illuminate\Support\Facades\Response::json([
+            'token' => auth()->user()->createToken('API Token')->plainTextToken
+        ], 200);
+
+    }
+
+    // this method signs out users by removing tokens
+    public function signout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Tokens Revoked'
+        ];
+    }
+
+    public function store_api(Request  $request){
+        $member = new Member();
+        $member->first_name = $request->first_name;
+        $member->middle_name = $request->middle_name;
+        $member->last_name = $request->last_name;
+        $member->id_number = $request->id_number;
+        $member->dob = $request->dob;
+        $member->gender = $request->gender;
+        $member->marital_status = $request->marital_status;
+        $member->phone = $request->phone;
+        $member->address = $request->address;
+        $member->save();
+        return \Illuminate\Support\Facades\Response::json($member, 200);
+
+    }
+
+    // this method returns all sermons
+    public function contributions($member_id) {
+        $data = \App\Contribution::where('member_id',$member_id)->where('status',1)->get();
+        return \Illuminate\Support\Facades\Response::json($data, 200);
+    }
+}
